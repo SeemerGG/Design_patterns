864dc4cad3f35b8473d5d1637fd637bdf106a77c
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-03-04 09:39:43 +0300

    Задание 1.8

--- Lab2/Student.rb
+++ Lab2/Student.rb
@@ -5,16 +5,37 @@ class Student
 
         @first_name, @last_name, @patronymic = first_name, last_name, patronymic
 
-        @telegram, @mail, @git, @id =  telegram, mail, git, id
-        @tel_num = Student.tel_num_corect(tel_num) ? tel_num : nil 
+        @telegram = Student.telegram_correct?(telegram) ? telegram : nil
+        @mail = Student.mail_correct?(mail) ? mail : nil
+        @git = Student.git_correct?(git) ? git : nil
+        @id = Student.id_correct?(id) ? id : nil
+        @tel_num = Student.tel_num_correct?(tel_num) ? tel_num : nil 
+
     end
 
+    
     def to_s
         return "#{@last_name}\n#{@first_name}\n#{@patronymic}\n#{@tel_num!=nil ? @tel_num+"\n": "" }#{@telegram!=nil ? @telegram+"\n": ""}#{@mail!=nil ? @mail+"\n" : ""}#{@git!=nil ? @git+"\n": ""}#{@id!=nil ? @id+"\n": ""}"
     end
     
-    def self.tel_num_corect(tel_num)
-       return /\+?\d{11,13}/ === tel_num 
+    def self.tel_num_correct?(var)
+       return /^\+?\d{11,13}/ === var 
+    end
+
+    def self.telegram_correct?(var)
+        return /^https:\/\/t.me\/*/ === var
+    end 
+
+    def self.mail_correct?(var)
+        return /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i === var
+    end
+
+    def self.git_correct?(var)
+        return /^https:\/\/*.*\/*/ === var
+    end
+
+    def self.id_correct?(var)
+        return /^[0-9]+$/ === var
     end
 
     def output_to_screen
--- Lab2/main.rb
+++ Lab2/main.rb
@@ -1,8 +1,8 @@
 require_relative 'Student'
 
 student1 = Student.new(last_name:"Курбатский", first_name:"Владимир", patronymic:"Александрович")
-student2 = Student.new(last_name:"Чутчев", first_name:"Сергей", patronymic:"Сергеевич", tel_num:"88005553535", git:"wintttr")
-student3 = Student.new(last_name:"Якухнов", first_name:"Роман", patronymic:"Андреевич", git:"RedMag")
+student2 = Student.new(last_name:"Чутчев", first_name:"Сергей", patronymic:"Сергеевич", tel_num:"88005553535")
+student3 = Student.new(last_name:"Якухнов", first_name:"Роман", patronymic:"Андреевич", git:"https://github.com/RedMag", id:"2345", telegram:"https://t.me/RedMag", mail:"ssdfgdf@gmail.com")
 
 #student1.output_to_screen
 #student2.output_to_screen
@@ -11,4 +11,4 @@ student3 = Student.new(last_name:"Якухнов", first_name:"Роман", patr
 student1.mail= "ddhm015@gmail.com"
 
 
-student2.output_to_screen
+student3.output_to_screen
